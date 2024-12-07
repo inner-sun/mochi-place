a145113e65e87962c68b0325118d9d0d477b84ab
InnerSun <167511709+inner-sun@users.noreply.github.com>
2024-12-07 18:16:20 +0100

    Added dev helpers and icons

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,32 @@
-import { defineConfig } from "@solidjs/start/config";
+import { defineConfig } from "@solidjs/start/config"
+import solidSvg from 'vite-plugin-solid-svg'
+import { undestructurePlugin } from 'babel-plugin-solid-undestructure'
 
-export default defineConfig({});
+export default defineConfig({
+  vite() {
+    return {
+      build: {
+        sourcemap: true
+      },
+      plugins: [
+        ...undestructurePlugin('ts'),
+        solidSvg({
+          svgo: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false
+                    },
+                  },
+                },
+              ],
+            }
+          }
+        }),
+      ],
+    }
+  }
+})
